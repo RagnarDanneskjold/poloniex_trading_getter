01e5f5cc9ce12dbdfa78e98adad6f73b698dfd54
Wendell Luckow <wluckow@gmail.com>
2017-06-20 14:45:29 -0600

    Update polo.py
    Changed labels to be the percent increase and decrease rather than the 3 classes (increase by x, decrease by y, or within x and y)

--- polo.py
+++ polo.py
@@ -35,47 +35,48 @@ def create_trade_data():
     sample_start = label_time - 2700 #look at 30 minute durations
     sample_end = label_time - 900 #look starting 15 minutes ago
     
-    for i in range(40000): #30k samples
+    for i in range(70000): #70k samples
     #for i in range(30):
 
         #collect data
         one_data = [] #the trade data for one chunk
-        #trade_current = polo.marketTradeHist(currencyPair = 'BTC_XMR', start = label_time - 20000, end = label_time)[0]
-        trade_current = polo.marketTradeHist(currencyPair = 'USDT_XMR', start = label_time - 20000, end = label_time)[0]
         
         try:
-            #look at the data from 10 minutes ago to 20 minutes ago
-            #trade_history = polo.marketTradeHist(currencyPair = 'BTC_XMR', start = sample_start, end = sample_end)
+            trade_current = polo.marketTradeHist(currencyPair = 'USDT_XMR', start = label_time - 20000, end = label_time)[0]
+            btc_current = polo.marketTradeHist(currencyPair = 'USDT_BTC', start = label_time - 20000, end = label_time)[0]
+            
             trade_history = polo.marketTradeHist(currencyPair = 'USDT_XMR', start = sample_start, end = sample_end)
+            usdt_btc_trade_history = polo.marketTradeHist(currencyPair = 'USDT_BTC', start = sample_start, end = sample_end)
             btc_xmr_trade_history = polo.marketTradeHist(currencyPair = 'BTC_XMR', start = sample_start, end = sample_end)
-            eth_xmr_trade_history = polo.marketTradeHist(currencyPair = 'XMR_DASH', start = sample_start, end = sample_end)
-            btc_eth_trade_history = polo.marketTradeHist(currencyPair = 'XMR_LTC', start = sample_start, end = sample_end)
-
-
+            xmr_dash_trade_history = polo.marketTradeHist(currencyPair = 'XMR_DASH', start = sample_start, end = sample_end)
+            xmr_ltc_trade_history = polo.marketTradeHist(currencyPair = 'XMR_LTC', start = sample_start, end = sample_end)
 
             rate = []
-            trade_times = []
+            btc_rate = []
 
             btc_xmr_buys = 0
             btc_xmr_bought = 0
             btc_xmr_sells = 0
             btc_xmr_sold = 0
 
-            eth_xmr_buys = 0
-            eth_xmr_bought = 0
-            eth_xmr_sells = 0
-            eth_xmr_sold = 0
+            xmr_dash_buys = 0
+            xmr_dash_bought = 0
+            xmr_dash_sells = 0
+            xmr_dash_sold = 0
 
-            btc_eth_buys = 0
-            btc_eth_bought = 0
-            btc_eth_sells = 0
-            btc_eth_sold = 0
+            xmr_ltc_buys = 0
+            xmr_ltc_bought = 0
+            xmr_ltc_sells = 0
+            xmr_ltc_sold = 0
 
             if len(trade_history) <= 4000 and len(trade_history) >= 2:
-                for trade in range(len(trade_history)): #look at all trades made in a 15 minute window
+                for trade in range(len(trade_history)): #look at all trades made in a 30 minute window
                     ###RATE###
                     moment_rate = float(trade_history[trade]['rate'])
                     rate.append(moment_rate)
+
+                    btc_moment_rate = float(usdt_btc_trade_history[trade]['rate'])
+                    btc_rate.append(btc_moment_rate)
 
                 ###BUYS AND SELLS###            
                 #NUMBER OF BUYS AND TOTAL AMOUNT BOUGHT 
@@ -86,47 +87,64 @@ def create_trade_data():
                     btc_xmr_sells += temp_btc_xmr_sells
                     btc_xmr_sold += temp_btc_xmr_sold
 
-                for trade in range(len(eth_xmr_trade_history)):
-                    temp_eth_xmr_buys, temp_eth_xmr_bought, temp_eth_xmr_sells, temp_eth_xmr_sold = bought_sold(eth_xmr_trade_history, trade)
-                    eth_xmr_buys += temp_eth_xmr_buys
-                    eth_xmr_bought += temp_eth_xmr_bought
-                    eth_xmr_sells += temp_eth_xmr_sells
-                    eth_xmr_sold += temp_eth_xmr_sold
+                for trade in range(len(xmr_dash_trade_history)):
+                    temp_xmr_dash_buys, temp_xmr_dash_bought, temp_xmr_dash_sells, temp_xmr_dash_sold = bought_sold(xmr_dash_trade_history, trade)
+                    xmr_dash_buys += temp_xmr_dash_buys
+                    xmr_dash_bought += temp_xmr_dash_bought
+                    xmr_dash_sells += temp_xmr_dash_sells
+                    xmr_dash_sold += temp_xmr_dash_sold
 
-                for trade in range(len(btc_eth_trade_history)):
-                    temp_btc_eth_buys, temp_btc_eth_bought, temp_btc_eth_sells, temp_btc_eth_sold = bought_sold(btc_eth_trade_history, trade)
-                    btc_eth_buys += temp_btc_eth_buys
-                    btc_eth_bought += temp_btc_eth_bought
-                    btc_eth_sells += temp_btc_eth_sells
-                    btc_eth_sold += temp_btc_eth_sold
+                for trade in range(len(xmr_ltc_trade_history)):
+                    temp_xmr_ltc_buys, temp_xmr_ltc_bought, temp_xmr_ltc_sells, temp_xmr_ltc_sold = bought_sold(xmr_ltc_trade_history, trade)
+                    xmr_ltc_buys += temp_xmr_ltc_buys
+                    xmr_ltc_bought += temp_xmr_ltc_bought
+                    xmr_ltc_sells += temp_xmr_ltc_sells
+                    xmr_ltc_sold += temp_xmr_ltc_sold
 
                 one_data.append(btc_xmr_buys)
                 one_data.append(btc_xmr_bought)
                 one_data.append(btc_xmr_sells)
                 one_data.append(btc_xmr_sold)
 
-                one_data.append(eth_xmr_buys)
-                one_data.append(eth_xmr_bought)
-                one_data.append(eth_xmr_sells)
-                one_data.append(eth_xmr_sold)
+                one_data.append(xmr_dash_buys)
+                one_data.append(xmr_dash_bought)
+                one_data.append(xmr_dash_sells)
+                one_data.append(xmr_dash_sold)
 
-                one_data.append(btc_eth_buys)
-                one_data.append(btc_eth_bought)
-                one_data.append(btc_eth_sells)
-                one_data.append(btc_eth_sold)
+                one_data.append(xmr_ltc_buys)
+                one_data.append(xmr_ltc_bought)
+                one_data.append(xmr_ltc_sells)
+                one_data.append(xmr_ltc_sold)
 
                 #Rate
-                #using a list of all the rates, calculate the min, max, and mean
-                min_rate = min(rate)
-                max_rate = max(rate)
-                rate_difference = max_rate - min_rate
-                one_data.append(rate_difference)
+                #using a list of all the rates, calculate the rate change, mean, and standard deviation
+                oldest_rate = rate[-1]
+                newest_rate = rate[0]
+                rate_percent_change = (newest_rate - oldest_rate) / oldest_rate
+                one_data.append(rate_percent_change)
+
+                rate = np.array(rate)
+                rate_mean = np.mean(rate)
+                rate_std = np.std(rate)
+                one_data.append(rate_mean)
+                one_data.append(rate_std)
+
+                oldest_btc_rate = btc_rate[-1]
+                newest_btc_rate = btc_rate[0]
+                rate_btc_percent_change = (newest_btc_rate - oldest_btc_rate) / oldest_btc_rate
+                one_data.append(rate_btc_percent_change)
+
+                btc_rate = np.array(btc_rate)
+                rate_btc_mean = np.mean(btc_rate)
+                rate_btc_std = np.std(btc_rate)
+                one_data.append(rate_btc_mean)
+                one_data.append(rate_btc_std)
 
                 #Volume
                 #Using the total amount sold and bought, and the time between the first and last purchase, calculate volume
                 vol2 = volume(btc_xmr_trade_history, btc_xmr_bought, btc_xmr_sold)
-                vol3 = volume(eth_xmr_trade_history, eth_xmr_bought, eth_xmr_sold)
-                vol4 = volume(btc_eth_trade_history, btc_eth_bought, btc_eth_sold)
+                vol3 = volume(xmr_dash_trade_history, xmr_dash_bought, xmr_dash_sold)
+                vol4 = volume(xmr_ltc_trade_history, xmr_ltc_bought, xmr_ltc_sold)
 
                 one_data.append(vol2)
                 one_data.append(vol3)
@@ -140,24 +158,15 @@ def create_trade_data():
                 rate_new = float(trade_current['rate'])
                 rate_old = float(trade_history[0]['rate'])
 
-                rate_increase = (rate_new - rate_old) / rate_old
-                rate_decrease = (rate_old - rate_new) / rate_old
-
-                if rate_increase >= 0.01: #if the price is projected to go up by at least 1 percent
-                    trade_labels.append([1, 0, 0])
-
-                elif rate_decrease >= 0.01: #if the price is projected to go down by at least 1 percent
-                    trade_labels.append([0, 0, 1])
-
-                else: #if the price does not go above or below one percent
-                    trade_labels.append([0, 1, 0])
+                percent_change = (rate_new - rate_old) / rate_old
+                trade_labels.append(percent_change)
 
             label_time = label_time - 660 #go back 11 minutes
             sample_start = label_time - 2700
             sample_end = label_time - 900
 
             iterator += 1
-            percent = int(iterator / 400)
+            percent = int(iterator / 700)
 
             sys.stdout.write("\r" + str(percent) + " percent complete" + " ||| " + str(trade_current['date']))
             sys.stdout.flush()
@@ -168,7 +177,7 @@ def create_trade_data():
             sample_end = label_time - 900
 
             iterator += 1
-            percent = int(iterator / 400)
+            percent = int(iterator / 700)
 
             sys.stdout.write("\r" + str(percent) + " percent complete" + " ||| " + str(trade_current['date']))
             sys.stdout.flush()
